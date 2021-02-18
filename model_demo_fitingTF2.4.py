bf8663c5990a4497e5468aa3881d08302b937e4f
wmy <1155130142@link.cuhk.edu.hk>
2021-02-18 17:37:57 +0800

    add k fold cross validation

--- model_demo_fitingTF2.4.py
+++ model_demo_fitingTF2.4.py
@@ -31,6 +31,7 @@ class ModelDemo:
         return [reduce_lr_loss, early_stop]
     #verbose = 1 为输出进度条记录
     #verbose = 0 为不在标准输出流输出日志信息
+    
 
     def train(self, binders, nonbinders,modelname):
         ### Data prep
@@ -83,6 +84,27 @@ hits_train = open('peptides_A0101_9.txt', mode='r').readlines()
 hits_train = [x.strip() for x in hits_train]
 decoys_train = open('decoys_train_9.txt', mode='r').readlines()
 decoys_train = [x.strip() for x in decoys_train]
+
+
+### 5folds cross validation
+# define 10-fold cross validation test harness
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cvscores = []
+for train, test in kfold.split(X, Y):
+  # create model
+    model = Sequential()
+    model.add(Dense(12, input_dim=8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # Fit the model
+    model.fit(X[train], Y[train], epochs=150, batch_size=10, verbose=0)
+    # evaluate the model
+    scores = model.evaluate(X[test], Y[test], verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    cvscores.append(scores[1] * 100)
+print("%.2f%% (+/- %.2f%%)" % (numpy.mean(cvscores), numpy.std(cvscores)))
 
 
 ### Train model
